b4c4824c35b2a8926d4674a1c138312276defd30
Tóth Tibor <toth.tibor@autsoft.hu>
2022-08-15 16:27:49 +0200

    IsSortable Tests

--- test/AutSoft.Linq.Tests/Queryable/OrderByExtensions/OrderByExtensionsTests.IsSortable.cs
+++ test/AutSoft.Linq.Tests/Queryable/OrderByExtensions/OrderByExtensionsTests.IsSortable.cs
@@ -1,5 +1,23 @@
-﻿namespace AutSoft.Linq.Tests.Queryable.OrderByExtensions;
+using AutSoft.Linq.Queryable;
+
+using FluentAssertions;
+
+using System.Reflection;
+
+namespace AutSoft.Linq.Tests.Queryable.OrderByExtensions;
 
 public partial class IsSortable : OrderByExtensionsTests
 {
+    public static TheoryData<PropertyInfo?, bool> SortablePropertyTheoryData => new()
+    {
+        { typeof(PersonDto).GetProperty(nameof(PersonDto.Age)), true },
+        { typeof(PersonDto).GetProperty(nameof(PersonDto.Address)), false },
+    };
+
+    [Theory]
+    [MemberData(nameof(SortablePropertyTheoryData))]
+    public void Should_ReturnCorrentIncication(PropertyInfo? propertyInfo, bool expectedResult)
+    {
+        propertyInfo!.IsSortable().Should().Be(expectedResult);
+    }
 }
