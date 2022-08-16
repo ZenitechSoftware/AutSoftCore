c1e1607f76ee1e9fff6e3e626f3de08627735a5a
Tóth Tibor <toth.tibor@autsoft.hu>
2022-08-16 15:13:28 +0200

    Where tests

---
+++ test/AutSoft.Linq.Tests/Queryable/WhereExtensions/WhereExtensionsTests.Where.cs
@@ -0,0 +1,53 @@
+using AutSoft.Linq.Queryable;
+
+using FluentAssertions;
+
+namespace AutSoft.Linq.Tests.Queryable.WhereExtensions;
+
+public partial class WhereExtensionsTests
+{
+    public class Where : WhereExtensionsTests
+    {
+        [Fact]
+        public void Should_AppendTruePredicate()
+        {
+            // Arrange
+            var subject = Enumerable.Range(1, 10).AsQueryable();
+
+            // Act
+            var result = subject.Where(true, x => x % 2 == 0).ToList();
+
+            // Assert
+            result.Should().HaveCount(subject.Count() / 2);
+            result.Should().AllSatisfy(x => x.Should().Match(i => i % 2 == 0));
+        }
+
+        [Fact]
+        public void Should_NotAppendTruePredicate()
+        {
+            // Arrange
+            var subject = Enumerable.Range(1, 10).AsQueryable();
+
+            // Act
+            var result = subject.Where(false, x => x % 2 == 0).ToList();
+
+            // Assert
+            result.Should().HaveCount(subject.Count());
+            result.Should().BeEquivalentTo(subject);
+        }
+
+        [Fact]
+        public void Should_AppendFalsePredicate()
+        {
+            // Arrange
+            var subject = Enumerable.Range(1, 10).AsQueryable();
+
+            // Act
+            var result = subject.Where(false, x => x % 2 == 0, x => x % 2 == 1).ToList();
+
+            // Assert
+            result.Should().HaveCount(subject.Count() / 2);
+            result.Should().AllSatisfy(x => x.Should().Match(i => i % 2 == 1));
+        }
+    }
+}
