a7deb0222025ef877e52a21647b3235cbc7b95a5
Stedra Kristóf <stedra.kristof@autsoft.hu>
2023-01-09 12:43:37 +0100

    Localize infinite scroll component

--- src/AutSoft.AspNetCore.Blazor/Localization/BlazorComponentLocalizer.cs
+++ src/AutSoft.AspNetCore.Blazor/Localization/BlazorComponentLocalizer.cs
@@ -1,14 +1,52 @@
 namespace AutSoft.AspNetCore.Blazor.Localization;
 
-
+/// <summary>
+/// Localization strings for blazor components.
+/// </summary>
 public class BlazorComponentLocalizer
 {
+    /// <summary>
+    /// Confirm dialog component close button label.
+    /// </summary>
     public virtual string ConfirmDialogCloseButtonLabel { get; } = "Close";
+
+    /// <summary>
+    /// Confirm dialog component yes button label.
+    /// </summary>
     public virtual string ConfirmDialogYesButtonLabel { get; } = "Yes";
+
+    /// <summary>
+    /// Filter root component header title.
+    /// </summary>
     public virtual string FilterRootHeaderTitle { get; } = "Filter";
+
+    /// <summary>
+    /// Filter root component refresh button label.
+    /// </summary>
     public virtual string FilterRootRefreshButtonLabel { get; } = "Refresh";
+
+    /// <summary>
+    /// Filter root component reset button label.
+    /// </summary>
     public virtual string FilterRootResetButtonLabel { get; } = "Reset";
+
+    /// <summary>
+    /// Error dialog component technical details label.
+    /// </summary>
     public virtual string ErrorDialogTechnicalDetailsLabel { get; } = "Technical details";
+
+    /// <summary>
+    /// Error dialog component close button label.
+    /// </summary>
     public virtual string ErrorDialogCloseButtonLabel { get; } = "Close";
+
+    /// <summary>
+    /// Loading view component technical details label.
+    /// </summary>
     public virtual string LoadingViewTechnicalDetailsLabel { get; } = "Technical details";
+
+    /// <summary>
+    /// Infinite scroll component no items content label.
+    /// </summary>
+    public virtual string InfiniteScrollNoItemsContentLabel { get; } = "No items to display";
 }
